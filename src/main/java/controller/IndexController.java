a7beaffb16bf69bc23c17974c81afddeaaf539e9
Jhordy Alexander Mora Tabares <jhoalmora91@gmail.com>
2023-10-30 22:15:43 +0100

    New Subject learnt: For each in thymeleaf

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,15 +1,13 @@
-package Controller;
+package controller;
 
-import Models.Usuario;
+import models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -23,23 +21,24 @@ public class IndexController {
 //    @RequestMapping(value = {"/index", "/inicio", "/"}, method = RequestMethod.GET) // esto se puede hacer para asigna el metodo a varios urls
 //    public String index(Model model) { // Model es una interfaz y ModelMap es una clase
 //    public String index(ModelMap model){
-    public String index(Map<String, Object> map){
+    public String index(Map<String, Object> map) {
 //        model.addAttribute("titulo", "Hola Spring Framework! Mi perrito"); // este metodo se usa con Model y ModelMap
         map.put("titulo", "Hola Spring Framework con map!");
         return "index";// aqui se pone el nombre del html que esta en templates sin la extencion. Spring lo encuentra automaticamente
     }
-// otra manera en con ModelAndView, esta es igual que el model anterior pero se le tiene que especificar con un metodo cual es la view(template) a usar
+
+    // otra manera en con ModelAndView, esta es igual que el model anterior pero se le tiene que especificar con un metodo cual es la view(template) a usar
     @RequestMapping(value = "/modelAndView", method = RequestMethod.GET)
-    public ModelAndView indexTwo(ModelAndView mv){
+    public ModelAndView indexTwo(ModelAndView mv) {
         mv.addObject("titulo", "Hola Spring Framework con modelAndView!");
         mv.setViewName("indexTwo");
         return mv;
     }
 
     @GetMapping(value = "/perfil")
-    public String perfil(Model model){
+    public String perfil(Model model) {
         Usuario usuario = new Usuario();
-        usuario.setNombre("Jhordy");
+        usuario.setNombre("Carmen");
         usuario.setApellido("Mora");
         usuario.setEmail("jhordy@mail.com");
         model.addAttribute("usuario", usuario);
@@ -47,9 +46,12 @@ public class IndexController {
 //        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre())); // otra manera de concatenar
         return "perfil";
     }
+
     @GetMapping(value = "/listar")
-    public String listar(Model model){
-        Collection<Usuario> usuarios = List.of();
+    public String listar(Model model) {
+        Collection<Usuario> usuarios = List.of(new Usuario("Juan", "Malo", "malo@mail.com")
+                , new Usuario("Orlando", "Ramirez", "ramirez@mail.com")
+                , new Usuario("Pedro", "Mora", "mora@mail.com"));
         model.addAttribute("titulo", "Listado de usuarios");
         model.addAttribute("usuarios", usuarios);
         return "listar";
--- src/main/java/models/Usuario.java
+++ src/main/java/models/Usuario.java
@@ -1,9 +1,18 @@
-package Models;
+package models;
 
 public class Usuario {
     private String nombre;
     private String apellido;
     private String email;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
 
     public String getEmail() {
         return email;
